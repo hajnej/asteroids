d5415dab5c10bce1306719bcb025743653c13006
Jakub Slatinsky <slatinskyj@gmail.com>
2025-09-14 10:07:18 +0200

    Refactor main loop to utilize sprite groups for updatable and drawable objects

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT/ 2)
     while True:
@@ -16,8 +19,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen,BLACK)
-        player.draw(screen)
-        player.update(dt)
+        for thing in drawable:
+          thing.draw(screen)
+        updatable.update(dt)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
